d2a1c6b7f4878ab3c265d10cb81cd2cd7c8620ca
Prajwal Kumar <94397677+prajwalyatharth@users.noreply.github.com>
2025-02-22 23:40:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ document.addEventListener("DOMContentLoaded", () => {
     
     // Start Game
     submitButton.addEventListener("click", () => {
-        player1 = document.getElementById("player-1").value.trim();
-        player2 = document.getElementById("player-2").value.trim();
+        player1 = document.getElementById("player1").value.trim();
+        player2 = document.getElementById("player2").value.trim();
         
         if (player1 === "" || player2 === "") {
             alert("Please enter names for both players!");
